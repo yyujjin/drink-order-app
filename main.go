84277f8c35bc2ee6a39c90255dd1487b56b77623
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-16 00:16:53 +0900

    get 라우터 생성 후 list.html연결

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
+	r.GET("/list", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "list.html", gin.H{})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
