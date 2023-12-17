51f9449b912f2550391a5e46d9b017ae4d522a7a
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-18 02:04:51 +0900

    중복된 아이템 선택 시 count 갯수 올리는 라우터 구현

--- main.go
+++ main.go
@@ -15,16 +15,17 @@ func main() {
 	type drinkItem struct {
 		Name  string
 		Price int
-		Src string
+		Src   string
+		Count int
 	}
 
 	var drinkItems = []drinkItem{
-		{"아메리카노", 4500,"../assets/images/Americano.png"},
-		{"카라멜 마끼아또", 6500,"../assets/images/CaramelMacchiato.png"},
-		{"민트초코 프라페", 6500,"../assets/images/MintChocolate Frappe.png"},
-		{"자몽 스무디", 5000,"../assets/images/Grapefruit Smoothie.png"},
-		{"카페 모카", 6000,"../assets/images/CafeMocha.png"},
-		{"레몬 에이드", 5000,"../assets/images/LemonAde.png"},
+		{"아메리카노", 4500, "../assets/images/Americano.png", 0},
+		{"카라멜 마끼아또", 6500, "../assets/images/CaramelMacchiato.png", 0},
+		{"민트초코 프라페", 6500, "../assets/images/MintChocolate Frappe.png", 0},
+		{"자몽 스무디", 5000, "../assets/images/Grapefruit Smoothie.png", 0},
+		{"카페 모카", 6000, "../assets/images/CafeMocha.png", 0},
+		{"레몬 에이드", 5000, "../assets/images/LemonAde.png", 0},
 	}
 	var cartItems = []drinkItem{}
 
@@ -38,32 +39,35 @@ func main() {
 
 	r.POST("/addToCart/:id", func(c *gin.Context) {
 		id, _ := strconv.Atoi(c.Param("id"))
-		fmt.Println(id)
+		for i := 0; i < len(cartItems); i++ {
+			if drinkItems[id].Name == cartItems[i].Name {
+				cartItems[i].Count += 1
+				fmt.Println(cartItems)
+				return
+			}
+		}
 		cartItems = append(cartItems, drinkItems[id])
 		fmt.Println(cartItems)
 	})
 
 	r.GET("/getCartItems", func(c *gin.Context) {
 		c.JSON(200, cartItems)
-		})
+	})
 
-	r.DELETE("/deleteItem/:id", func (c *gin.Context) {
-		id,err:= strconv.Atoi (c.Param("id"))  
-		// fmt.Println(id, err)
+	r.DELETE("/deleteItem/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
 			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "올바르지 않은 접근입니다."})
 			return
 		}
 		for index := range cartItems {
 			if index == id {
-				cartItems = append(cartItems[:index],cartItems[index+1:]...)
-				return  
+				cartItems = append(cartItems[:index], cartItems[index+1:]...)
+				return
 			}
 		}
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 	})
-	
-
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
