cd21f8c6ec610a939473fb535c34b354dfd37913
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-17 17:01:07 +0900

    list 페이지 음료 버튼 클릭시 배열에 저장되는 라우터 생성 후 js에 연결

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,2 +1,56 @@
-const body = document.querySelector("body")
-body.innerHTML = ""
+
+
+getCartItem()
+//라우터에 카트라우터에서 배열가져오는 함수 get
+async function getCartItem() {
+    const res = await fetch("http://localhost:8080/getItems")
+    const lists = await res.json()
+    console.log(lists)
+    makeDrinkList(lists)
+}
+
+
+//카트 라우터에서 저장된 숫자들을 기본 정보데이타에서 뽑아내서 form 으로
+//여기 js에 갖다줘서 그 갖다준거를 풀어서 list 작성하는 함수 
+function makeDrinkList(lists) {
+    const orderList = document.querySelector("#order-list")
+// orderList.innerHTML = ""
+    for (let i = 0; i < lists.length; i++) {
+        orderList.innerHTML += 
+        `<div class="lists">
+            <div class="drink-image" >
+                <img  src="../assets/images/no-image.jpg" width="100" height="100" alt="">
+            </div>
+            <ul>
+                <li>품명 : ${아메리카노}</li>  //lists[i].name
+                <li>가격 : ${4500}원</li>     //lists[i].price
+                <li>수량 : ${1} 개</li>       //lists[i].amount
+                <button class="delete-buttons">x</button>
+            </ul>
+        </div>`
+    }
+    const deleteButtons = document.querySelectorAll(".deleteButtons")
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function () {
+            deleteList(i)
+        })
+    }
+}
+
+
+//삭제 함수 
+async function deleteList (i) {
+    const confirmDelete = confirm("삭제하시겠습니까?")
+    if (!confirmDelete) {
+        return
+    }
+    try {
+        await fetch(`http://localhost:8080/delete/${i}`, {
+            method: "DELETE",
+        })
+        getCartItem()
+    } catch (error) {
+        console.error("네트워크 오류:", error)
+    }
+}
+ 
--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
+
+	type drinkItem struct {
+		Name  string
+		Price int
+	}
+
+	var drinkItems = []drinkItem{
+		{"아메리카노", 4500},
+		{"카라멜 마끼아또", 6500},
+		{"민트초코 프라페", 6500},
+		{"자몽 스무디", 5000},
+		{"카페 모카", 6000},
+		{"레몬 에이드", 5000},
+	}
+
+	var a = []drinkItem{}
+	//a = append(a, drinkItems[2])
+	//fmt.Println(a)
+
 	r.GET("/list", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "list.html", gin.H{})
 	})
+
+	r.GET("/cart", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "cart.html", gin.H{})
+	})
+	r.POST("/temp/:id", func(c *gin.Context) {
+		id, _ := strconv.Atoi(c.Param("id"))
+		fmt.Println(id)
+		a = append(a, drinkItems[id])
+		fmt.Println(a)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
