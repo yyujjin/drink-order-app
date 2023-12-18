d3a34588cb838c401a63ad38ae009562a694f099
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-18 15:34:23 +0900

    수량 조절 버튼 생성

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -23,7 +23,8 @@ function makeDrinkList(items) {
             <ul>
                 <li>품명 : ${items[i].Name}</li>  
                 <li>가격 : ${items[i].Price} 원</li>    
-                <li>수량 : ${items[i].Count} 개</li>      
+                <li>수량 : <button id="minus" class="count" >-</button>${items[i].Count}개
+                <button id="plus" class="count">+</button></li>      
                 <button class="delete-buttons">x</button>
             </ul>
         </div>`
