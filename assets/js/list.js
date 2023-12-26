3efcdab15f7b6e9241815bbf8bff40edcf76f1e9
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-26 12:27:12 +0900

    로컬스토리지에 저장되는 데이터 정보 축소

--- assets/js/list.js
+++ assets/js/list.js
@@ -17,7 +17,6 @@ for (let i = 0; i < drinks.length; i++) {
     })
 }
 
-
 function putItemToCart(i) {
     //배열, 객체는 const로 선언해도 내부 값을 바꿀 수 있음. 
     //const로 해봤자 값이 바뀌는데 let으로 해도 되는거 아니야? 라고 생각할 수 있지만 
@@ -30,11 +29,17 @@ function putItemToCart(i) {
     }
     try {
         let findItem = cartItems.findIndex(function (a) {
-            return a.Name == drinkItems[i].Name
+            return a.Id == i
+            //a==i 이렇게 안되는이유 findindex 속성이란?
+            //cartItems == i 같은 인덱스가 있으면 그거 반환해라 이렇게아닌건가?
         })
+        console.log(findItem)
 
         if (findItem == -1) {
-            cartItems.push(drinkItems[i])
+            //여기서 필요한 객체만 넘겨야되는데 
+            cartItems.push({
+                Id : i,
+                Count : drinkItems[i].Count})
         }else {
             cartItems[findItem].Count += 1
         }
@@ -52,8 +57,14 @@ function putItemToCart(i) {
     try {
         location.href = "http://localhost:8080/cart"
     } catch (error) {
-        console.error("네트워크 오류:", error)ㅇ
+        console.error("네트워크 오류:", error)
     }
 }
 
 // 로컬스토리지에 담긴 값으로 장바구니 화면 구현 하고 API 삭제하기 
+
+//그럼 완전 생처음에는 키값을 넣어줘야하나?
+// localStorage.setItem("cartItems","[]")
+// localStorage.setItem("cartItems","{}") 왜 이건 안됐나 
+
+
