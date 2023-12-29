6b3b57fa2bea688b845f32d93344f7a9993d8f17
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-12-30 06:14:02 +0900

    🔥 불필요 주석 삭제

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
 
-	//필드/ 데이터타입/ 컨텐츠타입
 	type drinkItem struct {
 		Id int `json:Id`
 		Name  string `json:"Name"`
@@ -20,9 +19,6 @@ func main() {
 		Src   string `json:"Src"`
 		Count int    `json:"Count"`
 	}
-// 이렇게 적음으로써 이 구조체는 json만 담을 수 있음 
-
-	//데이터를 body에 담아서 서버로 보내는 방법에는 form 태그,제이슨 등등이 있는데 
 
 	var drinkItems = []drinkItem{
 		{1,"아메리카노", 4500, "../assets/images/Americano.png", 1},
@@ -52,14 +48,9 @@ func main() {
 
 	r.POST("/sendCartItems", func(c *gin.Context) {
 		fmt.Println("정상")
-		var cartItems []drinkItem //js에서 배열로 넘겨줬기때문에 배열로 객체를 받아야한다.
-		if err := c.BindJSON(&cartItems); err != nil { //BindJSON => 제이슨을 객체로 바꿔주는거 ,실행하면 에러를 무조건 반환하는 함수 
-			// 실행해서 에러를 err변수에 담고 만약 err에 값이 담겼으면 return해라 는 거  
-			// err := c.BindJSON(&cartItems)
-			// if err != nil {
-
-			// }
-			// return 이걸 줄여쓴거 
+		var cartItems []drinkItem 
+		if err := c.BindJSON(&cartItems); err != nil { 
+			return  
 		}
 		fmt.Println(cartItems)
 	})
@@ -81,8 +72,5 @@ func main() {
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
-	// 리스트에서 음료를 추가 1 -> 장바구니에 담기 위해 서버에 추가
-	// 사용자가 장바구니에서 수량을 변경 5 -> 서버 X . 클라 내부적으로 수량 변경
-	// 주문하기 버튼을 누르면 최종 수량을 서버로 전달
 
 }
