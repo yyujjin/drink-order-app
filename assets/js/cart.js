35924fff9a797a826a1341697d8722e92121f7cf
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-01-15 23:39:32 +0900

    🐛 장바구니에 품목이 없을때 금액이 나타나는 오류 수정

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -11,27 +11,27 @@ async function getDrinkItems() {
     console.log(data)
     makeDrinkList()
 }
-let items = JSON.parse(localStorage.getItem("cartItems"))
-console.log(items)
+let cartItems = JSON.parse(localStorage.getItem("cartItems"))
+console.log(cartItems)
 
 function makeDrinkList() {
     const orderList = document.querySelector("#order-list")
     orderList.innerHTML = ""
 
-    if (items == null) {
+    if (cartItems == null) {
         return
     }
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0; i < cartItems.length; i++) {
         const foundItem = data.DrinkItems.find(function (a) {
-            return a.Id == items[i].Id
+            return a.Id == cartItems[i].Id
         })
         //option 선택할 수 있는 음료에만 radio 표시하기
         //id와 label for에 i를 넣어서 다른 id 생성했고
         //name명도 변경해줌
         const style = foundItem.Option == 0 ? "" : "display:none"
         // 선택된 옵션이 날아가지 않게 고정시킴 
-        const checkedHot = items[i].Option == 1 ? "checked" : ""
-        const checkedIce = items[i].Option == 2 ? "checked" : ""
+        const checkedHot = cartItems[i].Option == 1 ? "checked" : ""
+        const checkedIce = cartItems[i].Option == 2 ? "checked" : ""
        
         orderList.innerHTML += `<div class="lists">
             <div class="drink-image" >
@@ -40,7 +40,7 @@ function makeDrinkList() {
             <ul>
                 <li>품명 : ${foundItem.Name}</li>  
                 <li>가격 : ${foundItem.Price} 원</li>    
-                <li>수량 : <button class="minusCounts" data-index=${i} >-</button>${items[i].Count}개
+                <li>수량 : <button class="minusCounts" data-index=${i} >-</button>${cartItems[i].Count}개
                 <button class="plusCounts" data-index=${i}>+</button></li>
                 <div class="options">
                     <input id="hot${i}" type="radio" name="option${i}" value="1" data-index=${i} ${checkedHot} >
@@ -69,11 +69,11 @@ document.addEventListener("click", function (e) {
         //이벤트 리스너도 다르게 적용함
         //TODO 로컬스토리지도 변경 하기
     } else if (e.target.id.startsWith("hot")) {
-        items[e.target.dataset.index].Option = 1
-        localStorage.setItem("cartItems", JSON.stringify(items))
+        cartItems[e.target.dataset.index].Option = 1
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
     } else if (e.target.id.startsWith("ice")) {
-        items[e.target.dataset.index].Option = 2
-        localStorage.setItem("cartItems", JSON.stringify(items))
+        cartItems[e.target.dataset.index].Option = 2
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
     }
    
 })
@@ -82,19 +82,24 @@ async function deleteList(i) {
     if (!confirmDelete) {
         return
     }
-    items.splice(i, 1)
-    console.log(items)
-    localStorage.setItem("cartItems", JSON.stringify(items))
+    cartItems.splice(i, 1)
+    console.log(cartItems)
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
     makeDrinkList()
 }
 
 function totalPrice() {
     let total = 0
-    for (let i = 0; i < items.length; i++) {
+    if (cartItems==null) {
+        document.querySelector("#totalPay").innerHTML = total
+        return
+    }
+    for (let i = 0; i < cartItems.length; i++) {
+        
         const foundItem = data.DrinkItems.find(function (a) {
-            return a.Id == items[i].Id
+            return a.Id == cartItems[i].Id
         })
-        total += items[i].Count * foundItem.Price
+        total += cartItems[i].Count * foundItem.Price
     }
     document.querySelector("#totalPay").innerHTML = total
 }
@@ -104,38 +109,40 @@ async function orderDrinkItems() {
         return
     }
     //만약 option=0이면 alert $ return
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].Option == 0) {
+    for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].Option == 0) {
             alert("옵션을 선택해주세요.")
             return
         }
     }
+    
 
     await fetch(`http://localhost:8080/orderDrinkItems`, {
         // TODO API명 변경
         method: "POST",
-        body: JSON.stringify(items),
+        body: JSON.stringify(cartItems),
     })
     //주문하기 눌렀을 때 장바구니 비우기
-    items = null
+    cartItems = null
     localStorage.clear("cartItems")
     // localStorage.setItem("cartItems", JSON.stringify(items))
     makeDrinkList()
+    totalPrice()
 }
 
 function plusCount(selectedIndex) {
-    items[selectedIndex].Count += 1
-    localStorage.setItem("cartItems", JSON.stringify(items))
+    cartItems[selectedIndex].Count += 1
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
     makeDrinkList()
 }
 
 function minusCount(selectedIndex) {
-    if (items[selectedIndex].Count == 1) {
+    if (cartItems[selectedIndex].Count == 1) {
         alert("최소 주문 수량입니다.")
         return
     }
-    items[selectedIndex].Count -= 1
-    localStorage.setItem("cartItems", JSON.stringify(items))
+    cartItems[selectedIndex].Count -= 1
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
     makeDrinkList()
 }
 
